98d8a231ba6728c45bd211c486f793dbf11f43aa
yinlaenyo <120325015+yinlaenyo@users.noreply.github.com>
2023-04-03 11:14:26 +0630

    Add files via upload

---
+++ assignment10.c
@@ -0,0 +1,16 @@
+//
+// Created by National Cyber City on 2/6/2023.
+//
+
+
+#include "stdio.h"
+#include "assignment10.h"
+
+int main(){
+  space_counter();
+  loadingAllDataFromFile();
+  printingAllData();
+  welcome();
+
+    return 0;
+}
